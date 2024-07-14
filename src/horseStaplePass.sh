536c2dee80d808dd3172ceaa343aab419ff9b22d
Oliver Zenger <oliver@zenger.org>
2024-07-14 12:26:35 +0200

    check pwned func

--- src/horseStaplePass.sh
+++ src/horseStaplePass.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
+# Check if words.txt exists
 if [[ ! -f words.txt ]]; then
     echo "words.txt not found!"
     exit 1
 fi
 
-# function to get a random word
+# Function to get a random word
 get_random_word() {
     local WORD
     WORD=$(sort -R words.txt | head -n 1)
     echo "$WORD"
 }
 
-# function to generate a horse staple password
+# Function to generate a horse-staple password
 generate_horse_staple_password() {
     local NUM_WORDS="$1"
     local PASSWORD=""
@@ -29,4 +30,25 @@ generate_horse_staple_password() {
     echo "$PASSWORD"
 }
 
-generate_horse_staple_password "$1"
+# check pwned
+check_password_pwned () {
+    local PASSWORD="$1"
+    local SHA1
+    SHA1=$(echo -n "$PASSWORD" | shasum -a 1 | awk '{ print $1 }' | tr '[:lower:]' '[:upper:]')
+    local PREFIX=${SHA1:0:5}
+    local SUFFIX=${SHA1:5}
+
+    local RESPONSE
+    RESPONSE=$(curl -s "https://api.pwnedpasswords.com/range/$PREFIX")
+
+    local MATCH
+    MATCH=$(echo "$RESPONSE" | grep -i "$SUFFIX")
+    if [[ -n $MATCH ]]; then
+        local COUNT
+        COUNT=$(echo "$MATCH" | cut -d ':' -f 2 | tr -d '[:space:]')
+        echo "Oh no — pwned!"
+        echo "This password has been seen $COUNT times before and should never be used."
+    else
+        echo "The generated password is safe."
+    fi
+}
